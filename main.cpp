5d45d76a02a0b191be61e3ab8e1e53d9971cd848
unenfantseul <sjzmdiwanttosee@yeah.net>
2023-04-09 16:34:33 +0800

    docs:移除编译

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
+    button.resize(400, 100);
     button.show();
     std::cout<<""<<std::endl;
     return QApplication::exec();
